0490e62cb8ce259daaea01515c1951a32951c3ca
ivan koop <ivankoop3@gmail.com>
2017-11-29 11:14:16 -0300

    todo list terminado

--- testapp1/imports/components/todoList/todoList.js
+++ testapp1/imports/components/todoList/todoList.js
@@ -1,15 +1,41 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Tasks } from '../../api/tasks.js';
+import { Meteor } from 'meteor/meteor';
 import template from './todoList.html';
 
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
-        return Tasks.find({});
+        const selector = {};
+
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        return Tasks.find(selector, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
+      },
+      currentUser() {
+        return Meteor.user();
       }
     })
   }
@@ -18,11 +44,26 @@ class TodosListCtrl {
     // Insert a task into the collection
     Tasks.insert({
       text: newTask,
-      createdAt: new Date
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
     });
 
     // Clear form
     this.newTask = '';
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
   }
 
 }
