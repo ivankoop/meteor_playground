54b54f5713533ff4042748b752e5618c327b8896
ivan koop <ivankoop3@gmail.com>
2017-11-29 16:30:48 -0300

    mas cosas

--- testapp1/imports/components/todoList/todoList.js
+++ testapp1/imports/components/todoList/todoList.js
@@ -7,6 +7,7 @@ import template from './todoList.html';
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
+    this.subscribe('tasks');
 
     this.hideCompleted = false;
 
@@ -42,12 +43,7 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', newTask);
 
     // Clear form
     this.newTask = '';
@@ -55,15 +51,15 @@ class TodosListCtrl {
 
   setChecked(task) {
     // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      },
-    });
+     Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+     Meteor.call('tasks.remove', task._id);
+  }
+
+  setPrivate(task) {
+    Meteor.call('tasks.setPrivate', task._id, !task.private);
   }
 
 }
